d971be1e6474261adf726e826f4f5d6fefeb6669
Ataliya Aarfeen <89702899+Ataliya-01@users.noreply.github.com>
2022-10-19 12:52:17 +0530

    Add files via upload

---
+++ functionOddNumber.java
@@ -0,0 +1,22 @@
+//Write a function to print the sum of all odd numbers from 1 to n.
+import java.util.*;
+class functionOddNumber{
+	public static void OddNumber(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(i%2!=0){
+				sum=sum+i;
+				
+			}
+		}
+		System.out.println(sum);
+	}
+
+	public static void main(String args[]){
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		OddNumber(n);
+		
+	}
+}
+
